a5ed481313868e2cae477daeff93f778e358495a
wx_wx <152867998+wxxw2023@users.noreply.github.com>
2024-02-12 21:28:25 +0800

    Create 322_coin_change.rs

---
+++ 322_coin_change.rs
@@ -0,0 +1,17 @@
+impl Solution {
+    pub fn coin_change(coins: Vec<i32>, amount: i32) -> i32 {
+        let mut dp = vec![amount + 1; amount as usize + 1];
+        dp[0] = 0;
+        for coin in coins {
+            (coin as usize..=amount as usize).for_each(|i| {
+                dp[i] = dp[i].min(dp[i - coin as usize] + 1);
+            });
+        }
+        if dp[amount as usize] == amount + 1 {
+            -1
+        } else {
+            dp[amount as usize]
+        }
+
+    }
+}
